ea738fc29a9abe94ea21a3bc8bd74a038aab5c51
FunnyName <emi.houdart@student.uclouvain.be>
2026-03-19 12:21:30 +0100

    erased image to ascii and made a pixel sorting function as well as a binarize function

--- main.py
+++ main.py
@@ -1,34 +1,58 @@
 from PIL import Image
 
-path = input("Enter the path to the image :\n ")
-
-try:
-    with open(path) as img:
-        width, height = img.size
-        aspect_ratio = height/width
-        new_width = 120
-        new_height = aspect_ratio * new_width #* 0.55
-        img = img.resize((new_width, int(new_height)))
-
-        img = img.convert('L')
-
-        chars  = ["@","$","D","%","*","P","+","Y","J",",","."]
-
-        pixels = img.get_flattened_data()
-        new_pixels = [chars[pixel//25] for pixel in pixels]
-        new_pixels = ''.join(new_pixels)
-
-        new_pixels_count = len(new_pixels)
-        ascii_image = [new_pixels[index:index + new_width] for index in range (0, new_pixels_count, new_width)]
-        ascii_image = '\n'.join(ascii_image)
 
 
+#path = input("Enter the path to the image :\n ")
 
-        # write to a text file.
-        name = input("Enter the name of your ascii_art :\n ")
-        with open(name, "w") as f:
-            f.write(ascii_image)
-except:
-   print("Unable to find image")
+img = Image.open("Sil80.jpg")
+
+def pixelSortBrightness(brightness:int, pixels:int, img:Image, rgb:int):
+    img = img.convert('RGB')
+    if 0 <= rgb <= 2:
+        x = 0
+
+        while x < img.width:
+            y = 0
+            while y < img.height - pixels:
+                pixel = img.getpixel((x, y))
+                #print("(", x, ",",y,")",img.width, img.height, pixel)  
+
+                if pixel[rgb] >= brightness:
+                    list = []
+                    for i in range(pixels):
+                        list.append(img.getpixel((x, y+i)))
+
+                    list.sort()
+                    list.reverse()
+                    for i in range(len(list)):
+                        if y + i-1 < img.height:
+                            img.putpixel( (x, y + i-1), list[i] )
+                    y += pixels - 1
+                else:
+                    y += 1 
+            x += 1
+
+        return img
+
+
+def binarize(img, threshold):
+    output_image=img.convert("L")
+    for x in range(output_image.width):
+        for y in range(output_image.height):
+            if output_image.getpixel((x,y))< threshold:
+
+                output_image.putpixel( (x,y), 0 )
+            else:
+                output_image.putpixel( (x,y), 255 )
+
+    return output_image
+
+
+#img = binarize(img, 100)
+img = pixelSortBrightness(100, 200, img, 0)
+
+img.save("result.jpg")
+
+
 
 
